b6b0dabb155fc68926644630a387ee2164de18da
Isabella Mighell <ikm4dm@virginia.edu>
2021-09-03 08:57:43 -0400

    Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+function Keypad(){
+
+    return (
+        <input type="password" onChange={(e)=> console.log('Entering password...')}/>
+    )
+}
+
+export default Keypad;
